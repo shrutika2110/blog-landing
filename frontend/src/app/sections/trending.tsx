19cb8931dbb7092020f0bae3a44a2ae7b5cf899e
Saudamini <saudamini@geekyants.com>
2024-05-23 11:11:21 +0530

    added map

--- frontend/src/app/sections/trending.tsx
+++ frontend/src/app/sections/trending.tsx
@@ -6,6 +6,26 @@ import Heading from "@/components/composite/Heading";
 import { Card, CardBanner, CardContent, CardDate, CardTime, CardTitle } from "@/components/ui/card";
 import { MdOutlineAccessTime } from "react-icons/md";
 
+const cardData = [
+    {
+        src: "/images/blogs/blog2.svg",
+        title: "Hypertension- causes, complications and cure space for longer text",
+        date: "2 days ago",
+        time: "3 min"
+    },
+    {
+        src: "/images/blogs/blog2.svg",
+        title: "Hypertension- causes, complications and cure space for longer text",
+        date: "2 days ago",
+        time: "3 min"
+    },
+    {
+        src: "/images/blogs/blog2.svg",
+        title: "Hypertension- causes, complications and cure space for longer text",
+        date: "2 days ago",
+        time: "3 min"
+    }
+];
 
 export default function Trending() {
 
@@ -15,66 +35,28 @@ export default function Trending() {
                     Trending
                 </Heading>
                 <div className="grid lg:grid-cols-3 gap-5 my-5">
-                       <Card className="border-none bg-gray-250 rounded  ">
+                    {cardData.map((card, index) => (
+                        <Card key={index} className="border-none bg-gray-250 rounded">
                             <CardContent className="p-0">
-                                    <div className="h-48">
-                                        <CardBanner src="/images/blogs/blog2.svg" ></CardBanner>
-                                    </div>
-                                    <div >
-                                        <div className="p-3 xl:p-5">
-                                             <CardTitle>Hypertension- causes, complications and cure space for longer text</CardTitle>
-                                            <div className="flex gap-3 items-center">
-                                                <CardDate>2 days ago</CardDate>
-                                                <div className="h-1 w-1 bg-gray-350 rounded-full"></div>
-                                                <div className="flex items-center gap-1 text-gray-350">
-                                                    <MdOutlineAccessTime />
-                                                    <CardTime>3 min</CardTime>
-                                                </div>
+                                <div className="h-48">
+                                    <CardBanner src={card.src}></CardBanner>
+                                </div>
+                                <div>
+                                    <div className="p-3 xl:p-5">
+                                        <CardTitle>{card.title}</CardTitle>
+                                        <div className="flex gap-3 items-center">
+                                            <CardDate>{card.date}</CardDate>
+                                            <div className="h-1 w-1 bg-gray-350 rounded-full"></div>
+                                            <div className="flex items-center gap-1 text-gray-350">
+                                                <MdOutlineAccessTime />
+                                                <CardTime>{card.time}</CardTime>
                                             </div>
                                         </div>
                                     </div>
+                                </div>
                             </CardContent>
-                       </Card>
-                       <Card className="border-none bg-gray-250 rounded  ">
-                            <CardContent className="p-0">
-                                    <div className="h-48">
-                                        <CardBanner src="/images/blogs/blog2.svg"></CardBanner>
-                                    </div>
-                                    <div >
-                                        <div className="p-3 xl:p-5">
-                                             <CardTitle>Hypertension- causes, complications and cure space for longer text</CardTitle>
-                                            <div className="flex gap-3 items-center">
-                                                 <CardDate>2 days ago</CardDate>
-                                                <div className="h-1 w-1 bg-gray-350 rounded-full"></div>
-                                                <div className="flex items-center gap-1 text-gray-350">
-                                                    <MdOutlineAccessTime />
-                                                    <CardTime>3 min</CardTime>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                            </CardContent>
-                       </Card>
-                       <Card className="border-none bg-gray-250 rounded  ">
-                            <CardContent className="p-0">
-                                    <div className="h-48">
-                                        <CardBanner src="/images/blogs/blog2.svg"  ></CardBanner>
-                                    </div>
-                                    <div >
-                                        <div className="p-3 xl:p-5">
-                                             <CardTitle>Hypertension- causes, complications and cure space for longer text</CardTitle>
-                                            <div className="flex gap-3 items-center">
-                                                 <CardDate>2 days ago</CardDate>
-                                                <div className="h-1 w-1 bg-gray-350 rounded-full"></div>
-                                                <div className="flex items-center gap-1 text-gray-350">
-                                                    <MdOutlineAccessTime />
-                                                    <CardTime>3 min</CardTime>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                            </CardContent>
-                       </Card>
+                        </Card>
+                    ))}
                 </div>
             </div>
     );
