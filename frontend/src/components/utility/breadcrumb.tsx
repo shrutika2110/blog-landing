cd68cc611862262613cfd15d78d78eba1b593e5a
Saudamini <saudamini@geekyants.com>
2024-05-27 20:42:38 +0530

    added breadcrumb

--- frontend/src/components/utility/breadcrumb.tsx
+++ frontend/src/components/utility/breadcrumb.tsx
@@ -1,52 +1,36 @@
 import Link from 'next/link';
 import React from 'react';
+import { FaAngleRight } from 'react-icons/fa6';
 
-
-function Breadcrumb(props: any) {
+export default function Breadcrumb(props: any) {
   function isLast(index: any) {
     return index === props.crumbs.length - 1;
   }
 
   return (
-    <div className={`breadcrumb `}>
       <ul className="flex items-center">
-        
         {props.crumbs.map((crumb: any, ci: any) => {
           const disabled = isLast(ci);
-          return  (
+          return (
             <li key={ci} className="inline-flex items-center whitespace-nowrap">
               <Link href={`${disabled ? '#' : crumb.path}`}>
-                <div>
                   <p
-                    className={`  ${
-                      props.size == 'lg' ? 'text-md lg:text-lg' : 'text-lg'
-                    }  ${
+                    className={`text-lg font-extrabold ${
                       ci === 0
                         ? `  text-black-350 hover:text-primary`
                         : `  text-gray-750 `
-                    } ${disabled ? 'cursor-default' : 'cursor-pointer'} font-extrabold`}
+                    } ${disabled ? 'cursor-default' : 'cursor-pointer'}`}
                   >
                     {crumb.title}
                   </p>
-                </div>
               </Link>
-
-              <svg
-                className={`h-auto fill-current text-black-100 md:mx-1 md:w-4  lg:mx-1 lg:w-4 xl:mx-2 xl:w-5 ${
-                  disabled ? 'hidden' : 'block'
-                }  `}
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-              >
-                <path d="M0 0h24v24H0V0z" fill="none" />
-                <path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6-6-6z" />
-              </svg>
+              <div  className={`px-2 ${ disabled ? 'hidden' : 'block' } `}>
+                <FaAngleRight />
+              </div>
             </li>
           );
         })}
       </ul>
-    </div>
   );
 }
 
-export default Breadcrumb;
