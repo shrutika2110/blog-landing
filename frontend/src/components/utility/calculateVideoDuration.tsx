50506a74b0aa02eeccbb0bad8d6245c20c857d99
Saudamini <saudamini@geekyants.com>
2024-05-28 12:52:10 +0530

    warning removed

--- frontend/src/components/utility/calculateVideoDuration.tsx
+++ frontend/src/components/utility/calculateVideoDuration.tsx
@@ -1,39 +1,30 @@
 import React, { useEffect, useState } from 'react';
 
 function CalculateVideoDuration({ videoUrl }: any) {
-  const [videoTime, setVideoTime] = useState<number | null>(null);
   const [videoTimeDisplay, setVideoTimeDisplay] = useState<string | null>(null);
 
-  const fetchVideoDuration = () => {
-    const tempVideo = document.createElement('video');
-    tempVideo.src = videoUrl;
-
-    const duration = Math.floor(tempVideo.duration);
-    // console.log('temp', duration );
-
-    // tempVideo.onloadedmetadata = () => {
-    //   const duration = Math.floor(tempVideo.duration);
-      
-    //   setVideoTime(isNaN(duration) ? null : duration);
-
-    //   if (!isNaN(duration)) {
-    //     if (duration < 60) {
-    //       setVideoTimeDisplay(`${duration} secs`);
-    //     } else {
-    //       const minutes = Math.floor(duration / 60);
-    //       const seconds = duration % 60;
-    //       setVideoTimeDisplay(`${minutes}.${seconds} mins`);
-    //     }
-
-    //   } else {
-    //     setVideoTimeDisplay(null);
-    //   }
-
-    //   tempVideo.remove();
-    // };
-  };
-
   useEffect(() => {
+    const fetchVideoDuration = () => {
+      const tempVideo = document.createElement('video');
+      tempVideo.src = videoUrl;
+
+      tempVideo.onloadedmetadata = () => {
+        const duration = Math.floor(tempVideo.duration);
+        if (!isNaN(duration)) {
+          if (duration < 60) {
+            setVideoTimeDisplay(`${duration} secs`);
+          } else {
+            const minutes = Math.floor(duration / 60);
+            const seconds = duration % 60;
+            setVideoTimeDisplay(`${minutes}.${seconds} mins`);
+          }
+        } else {
+          setVideoTimeDisplay(null);
+        }
+        tempVideo.remove();
+      };
+    };
+
     fetchVideoDuration();
   }, [videoUrl]);
 
