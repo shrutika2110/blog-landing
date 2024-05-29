d90ea8e6c1e93f8e0b65e551cf49cdf252992935
Saudamini <saudamini@geekyants.com>
2024-05-29 14:46:29 +0530

    added image

--- frontend/src/app/sections/HeroSlider/sliderDetails.tsx
+++ frontend/src/app/sections/HeroSlider/sliderDetails.tsx
@@ -19,7 +19,7 @@ export default function SliderDetails({ BlogSliders }: SliderProps) {
                             <Image
                                 src={data?.attributes?.coverImg?.data?.attributes?.url || '/fallback-image.jpg'}
                                 alt={data.attributes?.Title || 'Blog Image'}
-                                layout="fill"
+                                fill={true}
                                 className='object-cover'
                             />
                         </div>
