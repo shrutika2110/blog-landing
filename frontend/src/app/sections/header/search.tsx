ca1ceeb316403a16e744bbb20882af4bd9c2917e
Saudamini <saudamini@geekyants.com>
2024-05-30 16:39:51 +0530

    added the data in the dropdown

--- frontend/src/app/sections/header/search.tsx
+++ frontend/src/app/sections/header/search.tsx
@@ -40,25 +40,10 @@ export default function Search() {
         <div className="h-9 w-9 bg-gray-150 rounded-full absolute right-0 top-px flex text-white justify-center items-center cursor-pointer transition-all duration-200 hover:bg-primary">
             <FaArrowRightLong size={16} />
         </div>
-        {query && <SearchDropdown />}
+        {isDropdownOpen && <SearchDropdown results={results} />}
     </div>
 
-    // <div className="relative w-7/12 lg:w-67.5">
-    //   <div className="absolute top-2 left-2.5">
-    //     <FiSearch className="text-primary" size={20} />
-    //   </div>
-    //   <div className="bg-search-grad rounded-lg p-px">
-    //     <Input
-    //       className="pl-10 pr-14 h-9 rounded-1.75 border-none "
-    //       placeholder="Search kōfuku"
-    //       value={query}
-    //       onChange={handleInputChange}
-    //     />
-    //   </div>
-    //   <div className="h-9 w-9 bg-gray-150 rounded-full absolute right-0 top-px flex text-white justify-center items-center cursor-pointer transition-all duration-200 hover:bg-primary">
-    //     <FaArrowRightLong size={16} />
-    //   </div>
-    // </div>
+    
   );
 }
 
--- frontend/src/app/sections/header/searchDropdown.tsx
+++ frontend/src/app/sections/header/searchDropdown.tsx
@@ -1,47 +1,31 @@
 import { Card, CardBanner, CardContent, CardDescription, CardTitle } from "@/components/ui/card";
 import Link from "next/link";
 
+interface Props {
+    results?: any;
+}
 
-export default function SearchDropdown() {
 
-    const blogPosts = [
-        {
-            href: '/blogs/vaccines-in-elderly-age',
-            title: 'Vaccines in Elderly Age',
-            src: '/images/poster.jpeg',
-            description: 'A short description about the blog, which is related to your search.'
-        },
-        {
-            href: '/blogs/healthy-eating',
-            title: 'Healthy Eating',
-            src: '/images/poster.jpeg',
-            description: 'Learn about healthy eating habits that can improve your lifestyle.'
-        },
-        {
-            href: '/blogs/exercise-tips',
-            title: 'Exercise Tips',
-            src: '/images/poster.jpeg',
-            description: 'Effective exercise tips for maintaining a healthy body.'
-        }
-    ];
-    
+export default function SearchDropdown({results}: Props) {
 
     return (
             <div className="absolute right-0 top-12 w-full min-w-[270px] h-50 overflow-y-auto bg-gray-900 rounded-md shadow-xl py-4 px-2.5">
                 <div className="flex flex-col gap-3 py-1">
-                        {blogPosts.map((post, index) => (
-                            <Link href={post.href} key={index} >
+                        {results && results.slice(0, 3).map((post:any, index:any) => (
+                            <Link href={"/blogs/" + post?.attributes?.slug } key={index} >
                                 <Card className="bg-transparent"  >
                                     <CardContent className="p-0">
                                         <div className="grid grid-cols-12">
                                             <div className="col-span-3">
-                                                <CardBanner src={post.src}></CardBanner>
+                                                <CardBanner src={post?.attributes?.coverImg?.data?.attributes?.url}></CardBanner>
                                             </div>
                                             <div className="col-span-9">
                                                 <div className="px-3">
-                                                    <CardTitle className="min-h-auto h-auto line-clamp-1 text-xs mb-1 font-medium font-overpass leading-3.75">{post.title}</CardTitle>
+                                                    <CardTitle className="min-h-auto h-auto line-clamp-1 text-xs mb-1 font-medium font-overpass leading-3.75">
+                                                    {post?.attributes?.Title}</CardTitle>
                                                     <div className="flex gap-3 items-center">
-                                                       <CardDescription className="text-xxs font-overpass leading-3.25 line-clamp-2" >{post.description}</CardDescription>
+                                                       <CardDescription className="text-xxs font-overpass leading-3.25 line-clamp-2" >
+                                                       {post?.attributes?.shortDes}</CardDescription>
                                                     </div>
                                                 </div>
                                             </div>
