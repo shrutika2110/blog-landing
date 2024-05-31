9c0ecdc77fe3fb71f8dd7a9a900ae8cc2d6e2d7e
Your Name <shrutika@geekyants.com>
2024-05-31 18:24:54 +0530

    Added privacy policy page

--- frontend/src/app/sections/footer/index.tsx
+++ frontend/src/app/sections/footer/index.tsx
@@ -15,21 +15,21 @@ export default function Footer() {
                         <Image src="/images/logoWhite.svg" alt="logo" fill={true} />
                     </div>
                     <div className="border border-white py-3 px-6 mt-14   flex flex-col gap-2.5 leading-7">
-                            <Text size="lg" className="font-extrabold">Disclaimer</Text>
-                            <Text>
-                                We are those little posters you stop and stare at, while mulling over the stock exchange
-                                – the ones that remind you of your dog’s wagging tail. We tell you the real things about
-                                health, while putting a smile on your face!
-                            </Text>
-                            <Text>
-                                Always seek proper professional help to diagnose and treat your medical conditions.
-                                Do not use the information we provide for self-diagnosis and self-cure.
-                            </Text>
+                        <Text size="lg" className="font-extrabold">Disclaimer</Text>
+                        <Text>
+                            We are those little posters you stop and stare at, while mulling over the stock exchange
+                            – the ones that remind you of your dog’s wagging tail. We tell you the real things about
+                            health, while putting a smile on your face!
+                        </Text>
+                        <Text>
+                            Always seek proper professional help to diagnose and treat your medical conditions.
+                            Do not use the information we provide for self-diagnosis and self-cure.
+                        </Text>
                     </div>
                     <div className="flex gap-4 justify-around items-center my-5">
                         <Link href="https://www.kofuku.com/" target="_blank" className="text-sm lg:text-lg font-semibold">About us</Link>
                         <Link href="/" className="text-sm lg:text-lg font-semibold">Terms & Conditions</Link>
-                        <Link href="/" className="text-sm lg:text-lg font-semibold">Privacy Policies</Link>
+                        <Link href="/privacy-policy" className="text-sm lg:text-lg font-semibold">Privacy Policies</Link>
                     </div>
                     <div className="flex gap-10 my-10 justify-center">
                         <SocialIconList />
@@ -38,7 +38,7 @@ export default function Footer() {
                         @ Kofuku Technologies
                     </Text>
                 </div>
-               
+
             </div>
         </footer>
     );
