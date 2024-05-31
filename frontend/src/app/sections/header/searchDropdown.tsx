087c45290786a664b14769982732aace10d3bc5f
Saudamini <saudamini@geekyants.com>
2024-05-31 17:16:09 +0530

    Added different

--- frontend/src/app/sections/header/searchDropdown.tsx
+++ frontend/src/app/sections/header/searchDropdown.tsx
@@ -15,38 +15,57 @@ export default function SearchDropdown({results}: Props) {
                 </div>
                 <div className="flex flex-col gap-3 py-1 relative z-10">
                         {results && results.slice(0, 3).map((post:any, index:any) => (
-                            <Link href={(post.type == "youtube" ? "/videos/" : "/blogs/") + post?.attributes?.slug } key={index} >
-                                <Card className="bg-gray-900"  >
-                                    <CardContent className="p-0">
-                                        <div className="grid grid-cols-12">
-                                            <div className="col-span-3">
-                                                {
-                                                post.type == "youtube" ? 
-                                                <div className="h-11 rounded overflow-hidden">
-                                                     <VideoIframe videoId={post?.attributes?.Link} size="xs" coverImg={post?.attributes?.CoverImg?.data?.attributes?.url} />
-                                                </div>:
-                                                <CardBanner src={post?.attributes?.coverImg?.data?.attributes?.url}></CardBanner> 
-                                                }
-                                            </div>
-                                            <div className="col-span-9">
-                                                <div className="px-3">
-                                                    <CardTitle className="min-h-auto h-auto line-clamp-1 text-xs mb-1 font-medium font-overpass leading-3.75">
-                                                    {post?.attributes?.Title}</CardTitle>
-                                                    <div className="flex gap-3 items-center">
-                                                       <CardDescription className="text-xxs font-overpass leading-3.25 line-clamp-2" >
-                                                       {
-                                                            post.type == "youtube" ? 
-                                                            post?.attributes?.Description:
-                                                            post?.attributes?.shortDes
-                                                        }
-                                                       </CardDescription>
+                            <div>
+                                {post.type == "youtube" ?
+                                        <Card className="bg-gray-900" key={index} >
+                                            <CardContent className="p-0">
+                                                <div className="grid grid-cols-12">
+                                                    <div className="col-span-3">
+                                                        <div className="h-11 rounded overflow-hidden">
+                                                            <VideoIframe videoId={post?.attributes?.Link} size="xs" coverImg={post?.attributes?.CoverImg?.data?.attributes?.url} />
+                                                        </div>
+                                                    </div>
+                                                    <div className="col-span-9">
+                                                    <Link href={"/videos/" + post?.attributes?.slug }  >
+                                                        <div className="px-3">
+                                                            <CardTitle className="min-h-auto h-auto line-clamp-1 text-xs mb-1 font-medium font-overpass leading-3.75">
+                                                            {post?.attributes?.Title}</CardTitle>
+                                                            <div className="flex gap-3 items-center">
+                                                            <CardDescription className="text-xxs font-overpass leading-3.25 line-clamp-2" >
+                                                            { post?.attributes?.Description }
+                                                            </CardDescription>
+                                                            </div>
+                                                        </div>
+                                                        </Link>
+                                                    </div>
+                                                </div>
+                                            </CardContent>
+                                        </Card>
+                                     : 
+                                    <Link href={ "/blogs/" + post?.attributes?.slug } key={index} >
+                                    <Card className="bg-gray-900"  >
+                                        <CardContent className="p-0">
+                                            <div className="grid grid-cols-12">
+                                                <div className="col-span-3">
+                                                        <CardBanner src={post?.attributes?.coverImg?.data?.attributes?.url}></CardBanner> 
+                                                </div>
+                                                <div className="col-span-9">
+                                                    <div className="px-3">
+                                                        <CardTitle className="min-h-auto h-auto line-clamp-1 text-xs mb-1 font-medium font-overpass leading-3.75">
+                                                        {post?.attributes?.Title}</CardTitle>
+                                                        <div className="flex gap-3 items-center">
+                                                            <CardDescription className="text-xxs font-overpass leading-3.25 line-clamp-2" >
+                                                            {post?.attributes?.shortDes}
+                                                            </CardDescription>
+                                                        </div>
                                                     </div>
                                                 </div>
                                             </div>
-                                        </div>
-                                    </CardContent>
-                                </Card>
-                            </Link>
+                                 </CardContent>
+                             </Card>
+                         </Link>
+                            }
+                            </div>
                         ))}
                     </div>
             </div>
