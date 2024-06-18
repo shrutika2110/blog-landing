1cea8c646a870d16d6845c0edcf1d91c694546fd
Your Name <shrutika@geekyants.com>
2024-06-18 19:32:31 +0530

    added padding

--- frontend/src/components/utility/MarkDownRenderer.tsx
+++ frontend/src/components/utility/MarkDownRenderer.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 import rehypeRaw from 'rehype-raw';
 
 interface MarkdownRendererProps {
-    content: any; 
+    content: any;
 }
 
 const MarkdownRenderer: React.FC<MarkdownRendererProps> = ({ content }) => {
@@ -18,32 +18,34 @@ const MarkdownRenderer: React.FC<MarkdownRendererProps> = ({ content }) => {
                     img: ({ node, ...props }) => (
                         <span className="block">
                             <Image
-                                src={String(props.src)} 
-                                width={600} 
-                                height={400} 
+                                src={String(props.src)}
+                                width={600}
+                                height={400}
                                 fill={false}
                                 alt="Image"
                             />
                         </span>
                     ),
-                blockquote: ({ node, ...props }) => (
-                    <blockquote className="mx-4 my-0 italic pl-4 border-l-4 border-offwhite-700 text-black-700" {...props} />
-                ),
-                code: ({ node, ...props }) => (
-                    <code className="text-wrap text-primary px-4 py-2 rounded text-sm bg-white-300" {...props} />
-                ),
-                ul: ({ node, ...props }) => <ul className="list-disc pl-8">{props.children}</ul>,
-                ol: ({ node, ...props }) => <ol className="list-decimal pl-8">{props.children}</ol>,
-                li: ({ node, ...props }) => <li className="mb-2">{props.children}</li>,
-                a: ({ node, ...props }) => (
-                    <a className="text-primary hover:text-sky-800  focus:text-sky-800 visited:text-sky-800" href={props.href}>
-                        {props.children}
-                    </a>
-                ),
-            }}
-        >
-            {content}
-        </ReactMarkdown>
+                    blockquote: ({ node, ...props }) => (
+                        <blockquote className="mx-4 my-0 italic pl-4 border-l-4 border-offwhite-700 text-black-700" {...props} />
+                    ),
+                    code: ({ node, ...props }) => (
+                        <code className="text-wrap text-primary px-4 py-2 rounded text-sm bg-white-300" {...props} />
+                    ),
+                    ul: ({ node, ...props }) => <ul className="list-disc pl-8">{props.children}</ul>,
+                    ol: ({ node, ...props }) => <ol className="list-decimal pl-8">{props.children}</ol>,
+                    li: ({ node, ...props }) => <li className="mb-2">{props.children}</li>,
+                    p: ({ node, ...props }) => <p className="mb-2">{props.children}</p>,
+
+                    a: ({ node, ...props }) => (
+                        <a className="text-primary hover:text-sky-800  focus:text-sky-800 visited:text-sky-800" href={props.href}>
+                            {props.children}
+                        </a>
+                    ),
+                }}
+            >
+                {content}
+            </ReactMarkdown>
         </div>
     );
 };
