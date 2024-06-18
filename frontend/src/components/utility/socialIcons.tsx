4a29810489340520c5151447473051d247c6a8a5
Saudamini <saudamini@geekyants.com>
2024-06-18 18:01:08 +0530

    Integrated social links

--- frontend/src/components/utility/socialIcons.tsx
+++ frontend/src/components/utility/socialIcons.tsx
@@ -7,47 +7,47 @@ import {
 import { FaGoogle, FaXTwitter, FaYoutube } from 'react-icons/fa6';
 
 interface Props {
-  socialData ?: any;
+  socialData?: any;
 }
 
-export default function SocialIconList({socialData}: Props) {
-    
+export default function SocialIconList({ socialData }: Props) {
   const SocialLinks = socialData;
-    
+
+  const order = ['google', 'facebook', 'instagram', 'youtube', 'linkedin', 'twitter'];
+
+  const sortedLinks = [...(SocialLinks || [])].sort((a, b) => {
+    return order.indexOf(a?.attributes?.title) - order.indexOf(b?.attributes?.title);
+  });
+
   return (
     <>
-      {SocialLinks &&
+      {sortedLinks && (
         <div className="flex gap-10 my-10 justify-center">
-            {
-                SocialLinks.map((data:any, index:any) => {
-                return (
-                    <>
-                      { data?.attributes?.link && 
-                        <div key={index}>
-                        <Link href={data?.attributes?.link } target="_blank" className='text-2xl lg:text-5.5xl'>
-                          { data?.attributes?.title == 'google' ? <FaGoogle  /> : 
-                            data?.attributes?.title == 'facebook' ?  <FaFacebook  /> :
-                            data?.attributes?.title == 'instagram' ?  <FaInstagram  /> :
-                            data?.attributes?.title == 'youtube' ?  <FaYoutube  /> :
-                            data?.attributes?.title == 'linkedin' ?  <FaLinkedin  /> :
-                            data?.attributes?.title == 'twitter' ?  <FaXTwitter  /> : null
-                          }
-                          
-                        </Link>
-                        </div>
-                      }
-                  </>
-                    
-                );
-            })}
-          </div>
-      }
+          {sortedLinks.map((data, index) => {
+            return (
+              data?.attributes?.link && (
+                <div key={index}>
+                  <Link href={data?.attributes?.link} target="_blank" className="text-2xl lg:text-5.5xl">
+                    {data?.attributes?.title === 'google' ? (
+                      <FaGoogle />
+                    ) : data?.attributes?.title === 'facebook' ? (
+                      <FaFacebook />
+                    ) : data?.attributes?.title === 'instagram' ? (
+                      <FaInstagram />
+                    ) : data?.attributes?.title === 'youtube' ? (
+                      <FaYoutube />
+                    ) : data?.attributes?.title === 'linkedin' ? (
+                      <FaLinkedin />
+                    ) : data?.attributes?.title === 'twitter' ? (
+                      <FaXTwitter />
+                    ) : null}
+                  </Link>
+                </div>
+              )
+            );
+          })}
+        </div>
+      )}
     </>
-    );
-  };
-
-    
-
-
-
-
+  );
+}
