16d43f74c303de63370d8a2c8750d45a99ed4cb8
Your Name <shrutika@geekyants.com>
2024-06-20 14:35:13 +0530

    changes

--- frontend/src/service/blogService.tsx
+++ frontend/src/service/blogService.tsx
@@ -1,14 +1,36 @@
+// import { BlogService } from "@/service";
+
+// export async function fetchBlogData() {
+//   try {
+
+//     const { data } = await BlogService();
+//     const content = data.blogs.data;
+//     return content
+//   }
+//   catch (e: any) {
+//     console.log("error:\n", e.message);
+
+//   }
+// }
+
+
+import { debounce } from "lodash";
 import { BlogService } from "@/service";
 
 export async function fetchBlogData() {
   try {
-
     const { data } = await BlogService();
     const content = data.blogs.data;
-    return content
-  }
-  catch (e: any) {
+    return content;
+  } catch (e: any) {
     console.log("error:\n", e.message);
-
   }
 }
+
+// Debounced version of fetchBlogData with a wait time of 300 milliseconds
+export const debouncedFetchBlogData = debounce(fetchBlogData, 300);
+
+// Usage example
+// You can call debouncedFetchBlogData() multiple times, but the actual fetchBlogData
+// will only be called once every 300 milliseconds.
+debouncedFetchBlogData();
