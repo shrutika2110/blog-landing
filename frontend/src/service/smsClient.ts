772c616673a8c0679367a015fab21018da84da4d
Your Name <shrutika@geekyants.com>
2024-06-20 13:02:08 +0530

    changes

--- frontend/src/service/smsClient.ts
+++ frontend/src/service/smsClient.ts
@@ -1,10 +1,9 @@
-
-
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 const token = process.env.NEXT_PUBLIC_STRAPI_API_TOKEN;
 
 const url = process.env.NEXT_PUBLIC_API_URL;
 const cmsClient = new ApolloClient({
+  ssrMode: true,
   uri: `${process.env.NEXT_PUBLIC_BASE_URL}/graphql`,
   cache: new InMemoryCache(),
   headers: {
@@ -13,48 +12,8 @@ const cmsClient = new ApolloClient({
   defaultOptions: {
     watchQuery: {
       fetchPolicy: "no-cache",
-      // pollInterval: 60000, // Poll data every 60 seconds (60000 milliseconds)
+      pollInterval: 60000, // Poll data every 60 seconds (60000 milliseconds)
     },
   },
 });
 export default cmsClient;
-
-
-// import { ApolloClient, InMemoryCache, HttpLink, ApolloLink, NormalizedCacheObject } from '@apollo/client';
-
-// const token = process.env.NEXT_PUBLIC_STRAPI_API_TOKEN;
-// const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
-
-// if (!token || !baseUrl) {
-//   throw new Error("Environment variables NEXT_PUBLIC_STRAPI_API_TOKEN and NEXT_PUBLIC_BASE_URL must be set");
-// }
-
-// const httpLink = new HttpLink({
-//   uri: `${baseUrl}/graphql`,
-//   fetch: (uri, options = {}) => {
-//     return fetch(uri, {
-//       ...options,
-//       headers: {
-//         ...options.headers,
-//         Authorization: `Bearer ${token}`,
-//       },
-//     });
-//   },
-// });
-
-// export const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
-//   cache: new InMemoryCache(),
-//   link: ApolloLink.from([httpLink]),
-//   defaultOptions: {
-//     query: {
-//       fetchPolicy: 'no-cache',
-//       errorPolicy: 'all',
-//     },
-//     watchQuery: {
-//       fetchPolicy: 'no-cache',
-//       errorPolicy: 'all',
-//     },
-//   },
-// });
-
-// export default client;
