e95ca4986e29a3bba4d1dd9ff0bc3adcfe7ac606
Saudamini <saudamini@geekyants.com>
2024-06-20 16:28:07 +0530

    added blogs

--- frontend/src/app/blogs/page.tsx
+++ frontend/src/app/blogs/page.tsx
@@ -3,10 +3,21 @@ import { BlogService } from "@/service";
 import BlogList from "./sections/blogList";
 
 import type { Metadata } from 'next'
-import { fetchBlogData } from "@/service/blogService";
 
 const commonTitle = 'Kofuku - Blog Library';
 const commonDescription = 'Kofuku is a one of a kind social media platform for healthcare. Talk about all things health, lifestyle and wellness by joining Kofuku and explore a content sharing search engine where you can read, write, share and more';
+
+async function fetchBlogData() {
+  try {
+      const { data } = await BlogService();
+      const content = data.blogs.data;
+      return content
+    }
+    catch (e: any) {
+      console.log("error:\n", e.message);
+    }
+}
+
 
 
 export const metadata: Metadata = {
