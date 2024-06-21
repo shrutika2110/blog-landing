7816023dfc7216a693d6cbd356dfcf21b597a54b
Your Name <shrutika@geekyants.com>
2024-06-21 11:34:51 +0530

    chanes

--- frontend/src/service/smsClient.ts
+++ frontend/src/service/smsClient.ts
@@ -3,17 +3,28 @@ const token = process.env.NEXT_PUBLIC_STRAPI_API_TOKEN;
 
 const url = process.env.NEXT_PUBLIC_API_URL;
 const cmsClient = new ApolloClient({
-  ssrMode: true,
   uri: `${process.env.NEXT_PUBLIC_BASE_URL}/graphql`,
   cache: new InMemoryCache(),
   headers: {
     Authorization: `Bearer ${token}`,
   },
   defaultOptions: {
+
     watchQuery: {
       fetchPolicy: "no-cache",
-      pollInterval: 60000, // Poll data every 60 seconds (60000 milliseconds)
+      errorPolicy: "ignore",
+    },
+    query: {
+      fetchPolicy: "no-cache",
+      errorPolicy: "all",
     },
   },
+  // defaultOptions: {
+  //   watchQuery: {
+  //     fetchPolicy: "no-cache",
+  //     pollInterval: 60000, // Poll data every 60 seconds (60000 milliseconds)
+  //   },
+  // },
+
 });
 export default cmsClient;
